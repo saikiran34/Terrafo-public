dcd7d26f00b5f1e6c9807768065b7958116cf040
saikiran34 <singaramsaikiran@gmail.com>
2022-05-17 16:27:38 -0400

    Add files via upload

---
+++ apigateway.tf
@@ -0,0 +1,45 @@
+resource "aws_api_gateway_rest_api" "Fxlink-Api" {
+  body = jsonencode({
+    openapi = "3.0.1"
+    info = {
+      title   = var.aws_api_gateway_rest_api_title
+      version = "1.0"
+    }
+    paths = {
+      "/path1" = {
+        get = {
+          x-amazon-apigateway-integration = {
+            httpMethod           = "GET"
+            payloadFormatVersion = "1.0"
+            type                 = "HTTP_PROXY"
+            uri                  = "https://ip-ranges.amazonaws.com/ip-ranges.json"
+          }
+        }
+      }
+    }
+  })
+
+  name = var.api_name
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+resource "aws_api_gateway_deployment" "Fxlink-Api_deploy" {
+  rest_api_id = aws_api_gateway_rest_api.Fxlink-Api.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.Fxlink-Api.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "Fxlink_Stage" {
+  deployment_id = aws_api_gateway_deployment.Fxlink-Api_deploy.id
+  rest_api_id   = aws_api_gateway_rest_api.Fxlink-Api.id
+  stage_name    = "v1"
+}
